55418b471a6a98d685da4729507c1b95cb338b83
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-15 14:58:52 +0530

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,11 @@
+Ram = 12 , 'June'
+Advay = (4 , 'March')
+Yuvan = (6 , 'May')
+Neil = (18 , 'October')
+Harleen = (27 , 'May')
+
+print(Ram)
+print(Advay)
+print(Yuvan)
+print(Neil)
+print(Harleen)
