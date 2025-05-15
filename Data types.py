297a50129626b6038969d4dbbf1b64446df49a08
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-15 14:38:28 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,12 @@
+x = 100
+y = "Advay"
+print(x)
+print(y, "\n")
+
+Codingal = 'codingal'
+print("I learned Python from", Codingal,  '\n')
+
+name = input("What is your name?:  ")
+age = int(input("What is your age?:  "))
+print("Hello", name, '\n')
+print("Bye, see you later")
