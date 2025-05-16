f7b77c6ebb90edf80af0959b4d048518e7b8b99e
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-16 14:35:48 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,6 @@
+# Get a string from the user
+string = str(input("Enter a string: "))
+print(string)
+revtext = string[::-1]
+print("The reverse of the given string is: ",revtext)
+print("Your original text was: ",string)
