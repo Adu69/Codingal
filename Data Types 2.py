996d2a317a453200378122576ead5296d2346f4a
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-16 14:15:53 +0530

    Add files via upload

---
+++ Data Types 2.py
@@ -0,0 +1,11 @@
+a = 60
+print('The type of variable a is:', type (a))
+
+b = 60.9
+print('The type of variable b is:', type (b))
+
+c = 'python'
+print('The type of variable c is:', type (c))
+
+d = False
+print('The type of variable d is:', type (d))
