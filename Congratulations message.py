de03bc95ac669c62e44229fe62f8cc2aca12f227
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-16 23:45:13 +0530

    Add files via upload

---
+++ Congratulations message.py
@@ -0,0 +1,4 @@
+friends_name = input("Enter your friends name: ")
+string = str(input(f"Enter a message you wish to convey to {friends_name} in lowercase lettering: "))
+uppercase = string.upper()
+print(uppercase)
