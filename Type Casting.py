95d0f4cebe7799db46835d837ca54e5e41fa44ba
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-16 14:28:33 +0530

    Add files via upload

---
+++ Type Casting.py
@@ -0,0 +1,26 @@
+name = "Advay"
+age = 12
+is_he_a_student = True
+height = 5.7
+
+print("Name: ", name)
+print("Data type of name: ", type(name))
+
+print("Age: ", age)
+print("Data type of name: ", type(age))
+
+print("Student: ", is_he_a_student)
+print("Data type of name: ", type(is_he_a_student))
+
+print("Height: ", height)
+print("Data type of name: ", type(height))
+
+#Now, Type Casting
+
+age = str(age)
+print(age)
+print('age data type = ', type(age))
+
+height = int(height)
+print('Height = ', height)
+print("Data type of height = ",type(height))
