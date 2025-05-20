1f882d69b006dcf9c7c3c6c391c306f74e483840
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-20 20:04:38 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,7 @@
+temp = int(input("Please enter the temperature in numerical format"))
+if temp > 30:
+    print('It is sunny')
+if temp < 30 & temp > 15 :
+    print('It is normal weather')
+if temp < 15:
+    print("It is very cold")
