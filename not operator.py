cc04c4c7206a4214929e32e583a4373acdbee069
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-21 14:26:04 +0530

    Add files via upload

---
+++ not operator.py
@@ -0,0 +1,23 @@
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+c = int(input("Enter a number"))
+
+print(a != b)
+print(b != c)
+
+
+a = 'python'
+b = 'coding'
+
+if a != b:
+    print(a,'and', b,'are different')
+
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+
+if (a==1) != (b==5):
+    print ('Hello')
+
+a = int(input("Enter a number"))
+if a%2 > 0:
+    print(a,"is an even number")
