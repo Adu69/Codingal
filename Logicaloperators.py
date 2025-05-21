e4ad1bfe972bc3a04e9267d470331fa78cacba2a
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-21 14:14:28 +0530

    Add files via upload

---
+++ Logicaloperators.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+if a and b and c:
+    print("All the variables are true")
+
+if a or b or c:
+    print("One or two of the variablees are true")
