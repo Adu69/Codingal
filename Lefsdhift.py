c8798721a6267525cdf65b0d3e95666434058f79
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-22 14:28:27 +0530

    Add files via upload

---
+++ Lefsdhift.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print(a << 1)
+print(b << 1)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print(a >> 1)
+print(b >> 1)
+print()
+input()
