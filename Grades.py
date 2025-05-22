a9692aa8061d1884c43083ddff598873ee033018
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-22 14:44:39 +0530

    Add files via upload

---
+++ Grades.py
@@ -0,0 +1,178 @@
+maths = int(input("Please enter marks for math in numerical format: "))
+physics = int(input("Please enter marks for physics in numerical format: "))
+biology = int(input("Please enter marks for biology in numerical format: "))
+chemistry = int(input("Please enter marks for chemistry in numerical format: "))
+english = int(input("Please enter marks for english in numerical format: "))
+hindi = int(input("Please enter marks for hindi in numerical format: "))
+marathi = int(input("Please enter marks for marathi in numerical format: "))
+sanskrit = int(input("Please enter marks for sanskrit in numerical format: "))
+hist = int(input("Please enter marks for hist in numerical format: "))
+geog = int(input("Please enter marks for geog in numerical format: "))
+
+if maths > 95:
+    print("A+")
+elif maths > 85:
+    print("A")
+elif maths > 75:
+    print("B+")
+elif maths > 70:
+    print("B")
+elif maths > 55:
+    print("C")
+elif maths > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if physics > 95:
+    print("A+")
+elif physics > 85:
+    print("A")
+elif physics > 75:
+    print("B+")
+elif physics > 70:
+    print("B")
+elif physics > 55:
+    print("C")
+elif physics > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if biology > 95:
+    print("A+")
+elif biology > 85:
+    print("A")
+elif biology > 75:
+    print("B+")
+elif biology > 70:
+    print("B")
+elif biology > 55:
+    print("C")
+elif biology > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if chemistry > 95:
+    print("A+")
+elif chemistry > 85:
+    print("A")
+elif chemistry > 75:
+    print("B+")
+elif chemistry > 70:
+    print("B")
+elif chemistry > 55:
+    print("C")
+elif chemistry > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if english > 95:
+    print("A+")
+elif english > 85:
+    print("A")
+elif english > 75:
+    print("B+")
+elif english > 70:
+    print("B")
+elif english > 55:
+    print("C")
+elif english > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if hindi > 95:
+    print("A+")
+elif hindi > 85:
+    print("A")
+elif hindi > 75:
+    print("B+")
+elif hindi > 70:
+    print("B")
+elif hindi > 55:
+    print("C")
+elif hindi > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if marathi > 95:
+    print("A+")
+elif marathi > 85:
+    print("A")
+elif marathi > 75:
+    print("B+")
+elif marathi > 70:
+    print("B")
+elif marathi > 55:
+    print("C")
+elif marathi > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if sanskrit > 95:
+    print("A+")
+elif sanskrit > 85:
+    print("A")
+elif sanskrit> 75:
+    print("B+")
+elif sanskrit > 70:
+    print("B")
+elif sanskrit > 55:
+    print("C")
+elif sanskrit > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if hist > 95:
+    print("A+")
+elif hist > 85:
+    print("A")
+elif hist > 75:
+    print("B+")
+elif hist > 70:
+    print("B")
+elif hist > 55:
+    print("C")
+elif hist > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
+
+
+
+if geog > 95:
+    print("A+")
+elif geog > 85:
+    print("A")
+elif geog > 75:
+    print("B+")
+elif geog > 70:
+    print("B")
+elif geog > 55:
+    print("C")
+elif geog > 50:
+    print("D")
+else:
+    print("FAIL, GO STUDY U MORON")
