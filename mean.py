4306407fb464dfe16cc3fbeb181892bba4ec8018
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-23 14:30:34 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+sum = mean1 * total_numbers
+correct_sum = (sum - wrong_number) + correct_number
+mean2 = correct_sum / 40 
+print("The correct mean is",mean2)
