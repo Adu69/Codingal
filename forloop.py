c7484471193f790f39da76ad7d17de86e1cc2b4b
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-05-27 14:57:15 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number you want to find the sum of: "))
+sum = 0
+for i in range(1 + num + 1):
+    sum = sum + 1
+    print("\nSum = ", sum)
---
+++ string.py
@@ -0,0 +1,7 @@
+string = input("Enter a string")
+string_2 = ('')
+for i in string:
+    string_2 = i + string_2
+
+print("The original string is ",string)
+print("The reversed string is",string_2)
