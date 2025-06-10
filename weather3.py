fe0986cbe7f46d055ecaaa379b06c87882f1d48f
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-06-10 17:00:37 +0530

    Add files via upload

---
+++ weather3.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("The weather conditions are pleasant in",spring)
+    print("The weather conditions are same in",autumn)
+
+spring = "autumn"
+autumn = spring
+weather_condition()
