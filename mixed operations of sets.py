bbe6c9d74568459cd69b0279d744618dbb191a56
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-07-03 16:46:11 +0530

    Add files via upload

---
+++ mixed operations of sets.py
@@ -0,0 +1,10 @@
+int_set = {1, 2, 3}
+print(int_set)
+print()
+mix_set = {1033, 3.32, 'Hello'}
+print(mix_set)
+print()
+dup_set = set([1, 2, 3, 4, 4 ,3, 2, 5])
+print(dup_set)
+print_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(print_set.pop)
